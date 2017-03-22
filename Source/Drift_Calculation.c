2c7d27616a6e3d3845ad0d6251b37e877cf6befb
Yuanchen Lu <ylu36@ncsu.edu>
2017-03-22 14:35:44 -0400

    use single-precision for sin and sqrt

--- Source/Drift_Calculation.c
+++ Source/Drift_Calculation.c
@@ -1,31 +1,19 @@
 //#include <MKL25Z4.H>
 
 #include <math.h>
-#include <arm_math.h>
+//#include <arm_math.h>
 #include "Drift_Calculation.h"
 #include "trig_approx.h"
 
-#define RAD 0.017453292
-#define RAD_INV 57.2957795
+#define RAD 0.0174532
+#define RAD_INV 57.29577
 //#define RAD_INV_Q 
 void Compute_Current(float speed_water, float angle_heading, float speed_ground, float angle_track, 
 	float * speed_current, float * angle_current){
 	float angle_drift_rad = 0.0;
 	float local_speed_current, local_angle_current;
-	float temp, rad_inv_q, q_180;
-	float oneenighty = 180;
-	float rad_inv = 57.2957795;
-	/*
-	q31_t q_speed_water, q_angle_heading, q_speed_ground, q_angle_track;
-	arm_float_to_q31(&speed_water, &q_speed_water, 1);
-	arm_float_to_q31(&angle_heading, &q_angle_heading, 1);
-	arm_float_to_q31(&speed_ground, &q_speed_ground, 1);	
-	arm_float_to_q31(&angle_track, &q_angle_track, 1);	
-	arm_float_to_q31(&rad_inv, &rad_inv_q, 1);
-	arm_float_to_q31(&oneenighty, &q_180, 1);
+	float temp;
 	
-  arm_sub_q31(&q_angle_track, &q_angle_heading, &temp);		*/
-	angle_drift_rad = (angle_track - angle_heading)*RAD;
 	if (fabs(speed_ground) < MIN_SPEED_TOLERANCE) { 		// Check for special cases - no relative motion
 		// not moving relative to ground, so current direction is opposite of heading through water
 		local_angle_current = angle_heading-180;
@@ -34,7 +22,10 @@ void Compute_Current(float speed_water, float angle_heading, float speed_ground,
 		// not moving relative to water, so current direction is track over ground
 		local_angle_current = angle_heading;
 		local_speed_current = speed_ground;
-	}	else if (fabs(angle_drift_rad) < MIN_ANGLE_TOLERANCE) { 	// Check for special cases - no angular difference
+	}	
+	else{
+	 angle_drift_rad = angle_track - angle_heading;
+	 if (fabs(angle_drift_rad) < MIN_ANGLE_TOLERANCE) { 	// Check for special cases - no angular difference
 		// track and heading are same: case IV
 		local_angle_current = angle_track;
 		local_speed_current = speed_ground - speed_water;
@@ -43,15 +34,16 @@ void Compute_Current(float speed_water, float angle_heading, float speed_ground,
 		local_angle_current = angle_heading-180;
 		local_speed_current = speed_ground + speed_water;
 	} else {
-		local_speed_current = sqrt(speed_ground*speed_ground + speed_water*speed_water - 2*speed_water*speed_ground*cos_32(angle_drift_rad));
-		temp = sin_32(angle_drift_rad)*speed_ground/local_speed_current;
+		local_speed_current = sqrtf(speed_ground*speed_ground + speed_water*speed_water - 2*speed_water*speed_ground*cos_32(angle_drift_rad*RAD));
+		temp = sin_32(angle_drift_rad*RAD)*speed_ground/local_speed_current;
 		if (temp > 1)
 			local_angle_current = 90 + angle_heading;
 		else if (temp < -1)
-			local_angle_current =  angle_heading - 90;
+			local_angle_current = 270+ angle_heading ;
 		else 
-			local_angle_current = (180-asin(temp)*RAD_INV) + angle_heading;
-	}	
+			local_angle_current = (180-asinf(temp)*RAD_INV) + angle_heading;
+		}	
+	}
 	if (local_angle_current < 0)
 		local_angle_current += 360;
 	if (local_angle_current >= 360)
